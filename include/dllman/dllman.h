5ada1e8e5e6f0e2184c8e9a8f2f2f2f1773929b8
Tetrago <fpcjt@outlook.com>
2020-03-22 18:51:09 -0400

    Refactor.

--- include/dllman/dllman.h
+++ include/dllman/dllman.h
@@ -1,18 +1,47 @@
 #pragma once
 
-#include <memory>
+#include <stdlib.h>
 
 #if defined(_MSC_VER)
 #define DLLMAN_WINDOWS
 #endif
 
-namespace dllman
+typedef enum DmMode
 {
-	enum class Support
-	{
-		Native
-	};
+	Native
+} DmMode;
 
-	template<Support T>
-	class Library {};
-}
+typedef struct DmLibrary
+{
+	/**
+	 * @brief	Load library by name. WILL unload existing library.
+	 *
+	 * @param	path	Path to library.
+	 *
+	 * @return	Success of load.
+	 */
+	int(*load)(struct DmLibrary*, const char*);
+
+	/**
+	 * @brief	Unload library.
+	 */
+	void(*unload)(struct DmLibrary*);
+
+	/**
+	 * @brief	Get a pointer from library.
+	 *
+	 * @param	name	Name to get.
+	 *
+	 * @return	Pointer or nullptr if not found.
+	 */
+	void*(*get)(struct DmLibrary*, const char*);
+
+	/**
+	 * @brief WILL unload loaded library.
+	 */
+	void(*free)(struct DmLibrary*);
+
+	void* data_;
+} DmLibrary;
+
+DmLibrary* dm_new(DmMode mode);
--- include/dllman/native.h
+++ include/dllman/native.h
@@ -1,40 +1,9 @@
 #pragma once
 
-#include <string>
-
 #include "dllman.h"
 
-namespace dllman
-{
-	template<>
-	class Library<Support::Native>
-	{
-	public:
-		~Library();
-
-		/**
-		 * @brief	Load library by name. WILL unload existing library.
-		 *
-		 * @param	path	Path to library.
-		 *
-		 * @return	Success of load.
-		 */
-		bool load(const std::string& path);
-
-		/**
-		 * @brief	Unload library. Destructor WILL unload automatically.
-		 */
-		void unload();
-
-		/**
-		 * @brief	Get a pointer from library.
-		 *
-		 * @param	name	Name to get.
-		 *
-		 * @return	Pointer or nullptr if not found.
-		 */
-		void* get(const std::string& name);
-	private:
-		void* library_ = nullptr;
-	};
-}
+DmLibrary* dm_native_new();
+void dm_native_free(DmLibrary* self);
+int dm_native_load(DmLibrary* self, const char* path);
+void dm_native_unload(DmLibrary* self);
+void* dm_native_get(DmLibrary* self, const char* name);
